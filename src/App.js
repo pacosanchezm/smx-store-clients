8c16abfaa6833ac48e8e24c1eb2f962c83c0a5fc
pacosanchezm <paco.sanchezm@gmail.com>
2021-12-01 10:44:54 -0600

    sucedere

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic pasare!</h2>
+      <h2>Start editing to see some magic sucedere!</h2>
     </div>
   );
 }
