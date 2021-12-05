4cfd700e3d53f356ee6fe9a4f2d24a9c4c77880a
pacosanchezm <paco.sanchezm@gmail.com>
2021-12-05 01:45:31 +0000

    w6mini

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic world6 sand!</h2>
+      <h2>Start editing to see some magic world ipadmini6 sand!</h2>
     </div>
   );
 }
